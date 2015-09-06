01bbd2ae8ceff16ccf6485b012a9bb00512c4578
Antonio Schmidt <antonio.schmidt@outlook.com>
2015-09-06 14:21:17 +0200

    build: intoducing useref for script and style assets, working on shared views.

--- gulp/build/pipes/html/viewPipe.js
+++ gulp/build/pipes/html/viewPipe.js
@@ -3,9 +3,15 @@ var requireDir = require('require-dir'),
     htmlPipes = requireDir('./');
 
 function _viewPipe(gulp, config, plugins) {
+    var useRef = plugins.useref,
+        assets = useRef.assets();
+
     return gulp
         .src(config.viewFiles)
         .pipe(htmlPipes.templatePipe(gulp, config, plugins)())
+        .pipe(assets)
+        .pipe(assets.restore())
+        .pipe(useRef())
         .pipe(plugins.if(config.optimize, htmlPipes.htmlOptimizationPipe(plugins)()))
         .pipe(gulp.dest(config.distPath));
 }
