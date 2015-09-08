c382ccee915651fa974a4b8aaf55671573e4a11e
Antonio Schmidt <antonio.schmidt@outlook.com>
2015-09-08 21:33:25 +0200

    build: main build pipe instead of mixed htmlPipe, html pipes fragmented, introduced js pipes (main js pipe and angular pipe)

--- src/shared/scripts/layout/main-controller.js
+++ src/shared/scripts/layout/main-controller.js
@@ -1,10 +1,12 @@
 angular.module('app').controller('mainController', function (sharedDataServiceA, sharedDataServiceB) {
     var self = angular.extend(this, {
-        navTitle: 'Toni angular build'
+        navTitle: 'Toni angular build',
+        sharedObjA: sharedDataServiceA.sharedDataServiceAObj,
+        sharedObjB: sharedDataServiceB.sharedDataServiceBObj
     });
 
     self.getNavTitle = function () {
-        return self.navTitle + ' ' + sharedDataServiceA.isSet + ' ' + sharedDataServiceB.isSet;
+        return self.navTitle + ' ' + self.sharedObjA.isSet + ' ' + self.sharedObjB.isSet;
     };
 
     return self;
